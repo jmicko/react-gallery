f4a2db01cf06a17547595eee4213e85aeabdca4b
John Micko <jrmicko5012@gmail.com>
2020-12-05 13:55:50 -0600

    add image

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,8 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
     { id: 2, path: 'images/xmas.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/pickle_dog.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 3, path: 'images/pickle_dog.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 4, path: 'images/sukkah.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
 ];
 
 module.exports = galleryItems;
