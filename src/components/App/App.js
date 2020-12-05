26925497395738a573556115b002bf5370a336fa
John Micko <jrmicko5012@gmail.com>
2020-12-05 02:02:52 -0600

    add GalleryList, get image list from server

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
+
 
 class App extends Component {
+
+  state = {
+    gallery: [],
+  }
+
+  componentDidMount(){
+    console.log('component did mount');
+    this.getAllPictures();
+  }
+
+  getAllPictures = () => {
+    axios.get('/gallery')
+      .then((response) => {
+        console.log('Response: ', response.data);
+        this.setState({
+          gallery: response.data
+        })
+      })
+      .catch((error) => {
+        alert('Something bad happened');
+        console.log('error', error);
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
+      <GalleryList gallery={this.state.gallery} />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" alt="goat" />
       </div>
     );
   }
---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,20 @@
+// imports
+import React, { Component } from 'react';
+
+// class
+class GalleryList extends Component {
+    render() {
+        console.log(this.props.gallery);
+        return (
+            <div>
+                <h2>hello from GalleryList component</h2>
+                {this.props.gallery.map(image => 
+                    <img src="image.path" alt="hey"></img>
+                    )}
+            </div>
+        ); // end return
+    } // end render
+} // end class
+
+// export
+export default GalleryList;
